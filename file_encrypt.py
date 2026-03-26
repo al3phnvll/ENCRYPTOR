6e0263dbafd0ecc6d8bb965f054f315f91d0661b
al3phnvll <alephnvll@gmail.com>
2026-03-26 21:15:13 +0100

    cli+non-cli-push

--- file_encrypt.py
+++ file_encrypt.py
@@ -3,30 +3,37 @@ from fernet_cipher import Crypt
 
 class FileOps:
     @staticmethod
-    def read_file(cls,path,key,choice):
+    def read_file(path,key,choice):
         encrypted_list = []
         with open(path, "r") as f:
             lines = f.readlines()
         if choice.lower() == "e":
             for line in lines:
-                byte_line = line.encode('utf-8')
-                print(Crypt.encrypt(byte_line, key))
+                clean = line.rstrip("\r\n")
+                if not clean:
+                    continue
+                byte_line = clean.encode('utf-8')
+                print(byte_line)
                 encrypted_list.append(Crypt.encrypt(byte_line,key))
         elif choice.lower() == "d":
             for line in lines:
-                byte_line = line.encode('utf-8')
-                print(Crypt.decrypt(byte_line,key))
+                clean = line.rstrip("\r\n")
+                if not clean:
+                    continue
+                byte_line = clean.encode('utf-8')
+                print(byte_line)
                 encrypted_list.append(Crypt.decrypt(byte_line,key))
+        for i in encrypted_list:
+            print(i)
         return encrypted_list
     
     @staticmethod
-    def write_file(cls,path,key,choice):
+    def write_file(path,key,choice):
         encrypted_list = FileOps.read_file(path,key,choice)
         with open(path , "w") as writefile:
             for i in encrypted_list:
                 print(i)
                 writefile.write(i)
                 writefile.write("\n")
-
 
 
